de9155aaad01532917c930bda84e6db73a25dda8
Dylan Quenneville <dquenne@gmail.com>
2020-06-07 16:20:44 -0700

    Clean up RequestManager

--- src/lib/requestManagement/RequestManager.ts
+++ src/lib/requestManagement/RequestManager.ts
@@ -2,77 +2,70 @@ import * as uuid from "uuid";
 import { CacheManager } from "../cache/CacheManager";
 
 export default class RequestManager<ResponseType, ArgumentsType extends any[]> {
-  readonly liveRequests: { [uniqueRequestId: string]: Promise<ResponseType> };
+  readonly liveRequests: { [requestId: string]: Promise<ResponseType> };
   readonly requestName: string;
 
   constructor(
     readonly cacheManager: CacheManager<ResponseType>,
-    readonly requestFunction: (...args: ArgumentsType) => Promise<ResponseType>,
-    requestName?: string
+    readonly requestFunction: (...args: ArgumentsType) => Promise<ResponseType>
   ) {
     this.liveRequests = {};
-    this.requestName = requestName || uuid.v4();
+    this.requestName = uuid.v4();
   }
 
   async call(...args: ArgumentsType) {
-    const uniqueRequestId = this.getUniqueRequestId(args);
+    const requestId = this.getRequestId(args);
 
     try {
-      const cached = await this.getFromCache(uniqueRequestId);
+      const cached = await this.cacheManager.get(requestId);
       if (cached) {
         return cached;
       }
 
-      const liveRequest = this.getLiveRequest(uniqueRequestId);
+      const liveRequest = this.getLiveRequest(requestId);
 
       if (liveRequest) {
         return await liveRequest;
       }
     } catch {}
 
-    return await this.makeNewRequest(uniqueRequestId, args);
+    return await this.makeNewRequest(requestId, args);
   }
 
-  getUniqueRequestId(args: ArgumentsType) {
+  private getRequestId(args: ArgumentsType) {
     return JSON.stringify([this.requestName, ...args]);
   }
 
   // new request management
 
-  async makeNewRequest(uniqueRequestId: string, args: ArgumentsType) {
+  private async makeNewRequest(requestId: string, args: ArgumentsType) {
     const requestPromise = this.requestFunction(...args);
 
-    this.setLiveRequest(uniqueRequestId, requestPromise);
+    this.setLiveRequest(requestId, requestPromise);
 
     const result = await requestPromise;
 
-    await this.cacheManager.set(uniqueRequestId, result);
+    await this.cacheManager.set(requestId, result);
 
-    this.clearLiveRequest(uniqueRequestId);
+    this.clearLiveRequest(requestId);
 
     return result;
   }
 
-  // cache management
-
-  async getFromCache(key: string) {
-    return await this.cacheManager.get(key);
-  }
-
   // live request management
 
-  setLiveRequest(
-    uniqueRequestId: string,
+  private setLiveRequest(
+    requestId: string,
     promise: Promise<ResponseType>
   ): void {
-    this.liveRequests[uniqueRequestId] = promise;
+    this.liveRequests[requestId] = promise;
   }
 
-  getLiveRequest(uniqueRequestId: string): Promise<ResponseType> | undefined {
-    return this.liveRequests[uniqueRequestId];
+  private getLiveRequest(requestId: string): Promise<ResponseType> | undefined {
+    return this.liveRequests[requestId];
   }
 
-  clearLiveRequest(uniqueRequestId: string) {
-    return delete this.liveRequests[uniqueRequestId];
+  private clearLiveRequest(requestId: string) {
+    return delete this.liveRequests[requestId];
   }
 }

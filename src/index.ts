076ba5f389d35d8edab535aa599b1a7ae301e476
Dylan Quenneville <dquenne@gmail.com>
2020-06-07 17:20:04 -0700

    Initial setup for npm package

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as RequestManager } from "./lib/requestManagement/RequestManager";
+export { CacheManager } from "./lib/cache/CacheManager";
+export { default as RedisCacheManager } from "./lib/cache/RedisCacheManager";
+export { default as SimpleCacheManager } from "./lib/cache/SimpleCacheManager";
